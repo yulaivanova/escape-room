58a4978df8869fc8d361ad7f232f52e44ce89711
yuliaivanova <yulialepehina@gmail.com>
2021-02-08 18:01:25 +0300

    Добавлена шапка сайта

--- gulpfile.js
+++ gulpfile.js
@@ -86,6 +86,7 @@ gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
+    "source/js/**",
     "source//*.ico"
     ], {
       base: "source"
