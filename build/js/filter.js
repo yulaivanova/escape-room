7ee197acd083e43fac0349a547c3682d19956d6f
yuliaivanova <yulialepehina@gmail.com>
2021-02-11 14:42:19 +0300

    Добавлен список квестов

--- build/js/filter.js
+++ build/js/filter.js
@@ -1,28 +1,28 @@
 'use strict';
 
 (function () {
-  const FILTER_LIST = document.querySelectorAll('.filter__item');
-  const ENTER_KEY = 13;
-  let filterItem;
+  const TAB_NAV = document.querySelectorAll('.filter__item');
+  var ENTER_KEY = 13;
+  let tabName;
 
-  FILTER_LIST.forEach(item => {
+  TAB_NAV.forEach(item => {
     item.addEventListener('click', selectTabNav);
     item.addEventListener('keydown', function (evt) {
-      filterItem = this.getAttribute('data-tab-name');
+      tabName = this.getAttribute('data-tab-name');
       if (evt.keyCode === ENTER_KEY) selectTabNavOnEnter(tabName);
     });
   });
 
   function selectTabNav() {
-    FILTER_LIST.forEach(item => {
+    TAB_NAV.forEach(item => {
       item.classList.remove('filter__item--active');
     });
     this.classList.add('filter__item--active');
   };
 
   function selectTabNavOnEnter() {
-    FILTER_LIST.forEach(item => {
-      item.dataset.filterItem === filterItem ? item.classList.add('filter__item--active') : item.classList.remove('filter__item--active');
+    TAB_NAV.forEach(item => {
+      item.dataset.tabName === tabName ? item.classList.add('filter__item--active') : item.classList.remove('filter__item--active');
     });
   };
 
